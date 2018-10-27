c994ce43582d4b3f1a25745075982de9aa0872bc
AnthonyFrancis <tonytrack@gmail.com>
2018-10-27 23:42:09 +0100

    added boostrapp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  get "blog" => "pages#blog"
+  get "books" => "pages#books"
+  get "news" => "pages#newsletter"
+  get "products" => "pages#products"
+  get "projects" => "pages#projects"
+  get "aboutme" => "pages#aboutme"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
